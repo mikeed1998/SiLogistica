17159d58a47b5fea8fc999c73559dd1feae5fc63
Michael <mikeed1998@gmail.com>
2024-05-08 11:23:38 -0600

    footer avanzado

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer>
+    <div class="container-fluid" style="background-color: #3567AC;">
+        <div class="row">
+            <div class="col-11 mx-auto">
+                <div class="row">
+                    <div class="col-lg-6 col-md-3 col-12 py-3">
+                        <div class="row">
+                            <div class="col-10 mx-auto text-md-end text-center">
+                                <img src="{{ asset('img/photos/header/logo_uno.PNG') }}" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-9 col-12 mt-md-4 mt-0 py-3">
+                        <div class="row">
+                            <div class="col" style="border-top: 3px solid #639BE8;">
+                                <div class="row mt-3 d-flex align-items-center justify-content-center text-center text-white fw-normal">
+                                    <div class="col-lg-5 col-md-4 col-12 py-3">
+                                        TEL. 3322332233
+                                    </div>
+                                    <div class="col-lg-5 col-md-4 col-12 text-md-end text-center py-3">
+                                        <div class="row">
+                                            <div class="col-lg-3 col-md-12 col-12"></div>
+                                            <div class="col-lg-9 col-md-12 col-12 mx-auto">
+                                                Av. Lazaro Cárdenas #33097 Chapalita C.P. 44500 Guadalajara, Jal.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-md-4 col-12 py-3">
+                                        <button type="button" onclick="activarModal()" class="btn btn-outline border-0 boton-menu">
+                                            <img src="{{ asset('img/photos/header/menubg.png') }}" alt="" class="img-fluid">
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col py-3" style="border-top: 3px solid #639BE8;">
+                        <div class="row d-flex align-items-center justify-content-center">
+                            <div class="col-md-4 col-6 text-start">
+                                <a href="#/" class="btn btn-outline border-0">
+                                    <i class="bi bi-whatsapp text-white fs-4"></i>
+                                </a>
+                                <a href="#/" class="btn btn-outline border-0">
+                                    <i class="bi bi-facebook text-white fs-4"></i>
+                                </a>
+                                <a href="#/" class="btn btn-outline border-0">
+                                    <i class="bi bi-instagram text-white fs-4"></i>
+                                </a>
+                            </div>
+                            <div class="col-md-4 col-12 d-md-block d-none"></div>
+                            <div class="col-md-4 col-6 text-end text-white">
+                                Si Logistica 2024 todos los derechos reservados, diseño por Wozial
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</footer>
