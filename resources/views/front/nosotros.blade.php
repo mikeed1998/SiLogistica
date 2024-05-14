4053c0040df01ecf3d35d318eaf8b266375f6de8
Michael <mikeed1998@gmail.com>
2024-05-14 17:00:09 -0600

    nosotros: ya finalize el circulo, fata lo responsive y concatenar los textos con el circulo

--- resources/views/front/nosotros.blade.php
+++ resources/views/front/nosotros.blade.php
@@ -6,7 +6,33 @@
     <style>
         
         body {
-            background-color: #3567AC; 
+            background-color:#3567AC; 
+        }
+
+        .row.bg-transparente {
+            position: relative;
+        }
+
+        .row.bg-transparente::before,
+        .row.bg-transparente::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            width: 100%;
+            z-index: -1;
+        }
+
+        .row.bg-transparente::before {
+            top: 0;
+            height: 20%;
+            background-color: #3567AC; /* Azul */
+        }
+
+        .row.bg-transparente::after {
+            top: 20%;
+            height: 80%;
+            background-color: white; /* Blanco */
         }
 
     </style>
@@ -14,22 +40,22 @@
 
 @section('content')
     <section>
-        <div class="container-fluid">
+        <div class="container-fluid" >
             <div class="row">
                 <div class="col">
-                    <div class="row">
-                        <div class="col-6" style="background-color: #3567AC;">
-                            <div class="row">
+                    <div class="row bg-transparente">
+                        <div class="col-6">
+                            <div class="row" style="background-color: #3567AC;">
                                 <div class="col-10 text-white py-5 mx-auto display-2 fw-bolder text-end border">
                                     Empresa 100% Mexicana
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col border bg-white h-100">
+                                <div class="col border bg-white" style="height: 33rem;">
                                     <div class="row">
                                         <div class="col-10 mx-auto">
                                             <div class="row">
-                                                <div class="col-9 ms-auto py-5" style="line-height: 1; hyphens: auto; text-align: right; font-size: 0.9rem;">
+                                                <div class="col-9 ms-auto py-5" style="line-height: 1; hyphens: auto; text-align: right; font-size: 1rem;">
                                                     SI LOGISTICA es una empresa 100% mexcicana que nace en 2019, especializandose en logística y transporte de carga, siendo un proveedor que se adecua a las necesidades de los clientes para garantizar la satisfacción de los mismos (desde su recolección hasta el destino final) y asi tener una efectiva cadena de suministros cumpliendo a detalle en tiempo y forma cada uno de los compromisos adquiridos.
                                                     <br><br>
                                                     Nuestra experiencia nos ha permitido trabajar con empresas líderes en el mercado en México y en el extranjero. El origen de nuestra motivación está en ofrecer a nuestros clientes soluciones a medida en servicios con caja seca, transportando componentes electrónicos, materia prima, producto terminado, alimnetos veterinarios, consumibles de industria farmacéutica, suplementos alimenticios, equipo médico, refacciones automovilísticas, cargas de alto valor (bolsas, calzado) entre otros. Asi como entregas especificas para el sector gobierno.
@@ -54,8 +80,154 @@
             </div>
         </div>
     </section>
+
+    <section>
+        <div class="container-fluid bg-white">
+            <div class="row">
+                <div class="col py-5 border">
+                    <div class="row" style="height: 40rem;">
+                        <div class="col-6 border py-5">
+                            <div class="row">
+                                <div class="col-6 mx-auto py-5 border">
+                                    <div class="row">
+                                        <div class="col display-2 fw-bolder" style="color: #D0382A;">
+                                            &nbsp;Misión
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-11" style="border-bottom:5px solid #3567AC;"></div>
+                                        <div class="col-1">
+                                            <div class="row">
+                                                <div class="col-3"></div>
+                                                <div class="col-9 pt-4" style="background-color: #3567AC;"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col py-3" style="hyphens: auto; text-align: justify; line-height: 1;">
+                                            Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. odit at. Et pariatur sunt adipisci magni. dolor sit amet consectetur adipisicing elit. Inventore neque unde sed eum voluptatem nam? Cum maiores totam officiis repellat?  
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 border py-5">
+                            <div class="row">
+                                <div class="col-6 mx-auto py-5 border">
+                                    <div class="row">
+                                        <div class="col display-2 fw-bolder" style="color: #D0382A;">
+                                            &nbsp;Visión
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-11" style="border-bottom:5px solid #3567AC;"></div>
+                                        <div class="col-1">
+                                            <div class="row">
+                                                <div class="col-3"></div>
+                                                <div class="col-9 pt-4" style="background-color: #3567AC;"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col py-3" style="hyphens: auto; text-align: justify; line-height: 1;">
+                                            Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. odit at. Et pariatur sunt adipisci magni. dolor sit amet consectetur adipisicing elit. Inventore neque unde sed eum voluptatem nam? Cum maiores totam officiis repellat?  
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container-fluid bg-white pt-5">
+        <div class="row" style="background: linear-gradient(to bottom, #FFFFFF 50%, #D0382A 50%);">
+            <div class="col pt-2 border border-dark rounded-circle bg-white">
+                <div class="row" style="height: 1000px;">
+                    <div class="col-6 mx-auto py-2 border border-primary position-relative">
+                        <div class="col-12 position-absolute bottom-0 start-0 border border-warning">
+                            <img src="{{ asset('img/photos/nosotros/nosotros_stockchida.png') }}" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col py-5 border" style="background-color: #D0382A;">
+
+            </div>
+        </div>
+    </div>
+
+    <section class="slider-logos_container">
+        <div class="container-fluid" style="background-color: #F6F6F6;">
+            <div class="row text-center py-5 d-flex align-items-center justify-content-center">
+                <div class="slider-logos">
+                    <div class="col mx-auto">
+                        <img src="{{ asset('img/photos/home/DHL.png') }}" alt="" class="img-fluid p-md-1 p-2" style="width: 100%; height: 3rem; object-fit: contain;">
+                    </div>
+                    <div class="col mx-auto">
+                        <img src="{{ asset('img/photos/home/FEDEX.png') }}" alt="" class="img-fluid p-md-1 p-2" style="width: 100%; height: 3rem; object-fit: contain;">
+                    </div>
+                    <div class="col mx-auto">
+                        <img src="{{ asset('img/photos/home/REDPACK.png') }}" alt="" class="img-fluid p-md-1 p-2" style="width: 100%; height: 3rem; object-fit: contain;">
+                    </div>
+                    <div class="col mx-auto">
+                        <img src="{{ asset('img/photos/home/REDPACK.png') }}" alt="" class="img-fluid p-md-1 p-2" style="width: 100%; height: 3rem; object-fit: contain;">
+                    </div>
+                    <div class="col mx-auto">
+                        <img src="{{ asset('img/photos/home/REDPACK.png') }}" alt="" class="img-fluid p-md-1 p-2" style="width: 100%; height: 3rem; object-fit: contain;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
 
 @section('scripts')
-    
+    <script>
+
+        $('.slider-logos').slick({
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: false,
+            nextArrow: false,
+            responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    prevArrow: false,
+                    nextArrow: false,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    prevArrow: false,
+                    nextArrow: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: false,
+                    nextArrow: false
+                }
+            }
+            ]
+        });
+
+    </script>
 @endsection
